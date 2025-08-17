d2ae7ff5f640c77ca1d01005a4b56fd20c91da7f
Manoel <manoel.carvalhoneto@tutamail.com>
2025-08-16 22:52:41 -0300

    feat(nav): Fetch customer and organization data for nav user

--- apps/dashboard/src/layout/nav-user.tsx
+++ apps/dashboard/src/layout/nav-user.tsx
@@ -25,7 +25,7 @@ import {
    useSidebar,
 } from "@packages/ui/components/sidebar";
 import { Skeleton } from "@packages/ui/components/skeleton";
-import { useQueryClient } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { Link, useRouter } from "@tanstack/react-router";
 import {
    LogOutIcon,
@@ -101,6 +101,12 @@ function NavUserContent({ session }: { session: Session | null }) {
    const router = useRouter();
    const trpc = useTRPC();
    const queryClient = useQueryClient();
+   const { data: customer } = useSuspenseQuery(
+      trpc.authHelpers.getCustomerState.queryOptions(),
+   );
+   const { data: organization } = useSuspenseQuery(
+      trpc.authHelpers.getDefaultOrganization.queryOptions(),
+   );
    const handleLogout = useCallback(async () => {
       await betterAuthClient.signOut(
          {},
@@ -172,33 +178,37 @@ function NavUserContent({ session }: { session: Session | null }) {
                            </Link>
                         </Button>
                      </DropdownMenuItem>
-                     <DropdownMenuItem asChild>
-                        <Button
-                           className="w-full items-center cursor-pointer justify-start flex gap-2 h-12"
-                           variant="ghost"
-                           onClick={() => setOpenMobile(false)}
-                           asChild
-                        >
-                           <Link to="/apikey">
-                              <KeyIcon />
-                              Api keys
-                           </Link>
-                        </Button>
-                     </DropdownMenuItem>
-                     <DropdownMenuItem asChild>
-                        <Button
-                           className="w-full items-center cursor-pointer justify-start flex gap-2 h-12"
-                           variant="ghost"
-                           onClick={() => setOpenMobile(false)}
-                           asChild
-                        >
-                           <Link to="/organization">
-                              <Building2 />
-                              Organizations
-                           </Link>
-                        </Button>
-                     </DropdownMenuItem>
-                  </DropdownMenuGroup>{" "}
+                     {customer.activeSubscriptions && (
+                        <DropdownMenuItem asChild>
+                           <Button
+                              className="w-full items-center cursor-pointer justify-start flex gap-2 h-12"
+                              variant="ghost"
+                              onClick={() => setOpenMobile(false)}
+                              asChild
+                           >
+                              <Link to="/apikey">
+                                 <KeyIcon />
+                                 Api keys
+                              </Link>
+                           </Button>
+                        </DropdownMenuItem>
+                     )}
+                     {(customer.activeSubscriptions || organization) && (
+                        <DropdownMenuItem asChild>
+                           <Button
+                              className="w-full items-center cursor-pointer justify-start flex gap-2 h-12"
+                              variant="ghost"
+                              onClick={() => setOpenMobile(false)}
+                              asChild
+                           >
+                              <Link to="/organization">
+                                 <Building2 />
+                                 Organizations
+                              </Link>
+                           </Button>
+                        </DropdownMenuItem>
+                     )}
+                  </DropdownMenuGroup>
                   <DropdownMenuSeparator />
                   <DropdownMenuItem asChild>
                      <Button
