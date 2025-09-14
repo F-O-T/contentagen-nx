a8ecfa2eba9011c594d072ef3ba1faed7beb1ea4
Manoel <manoel.carvalhoneto@tutamail.com>
2025-09-14 09:41:08 -0300

    feat(dashboard): implement localization for dashboard stats

---
+++ packages/localization/src/index.ts
@@ -0,0 +1,44 @@
+import i18n from "i18next";
+import enUSResources from "./locales/en-US";
+
+type RecursiveKeyOf<T> = T extends object
+   ? {
+        [K in keyof T]: K extends string
+           ? T[K] extends object
+              ? `${K}.${RecursiveKeyOf<T[K]>}` | K
+              : K
+           : never;
+     }[keyof T]
+   : never;
+
+type TranslationResources = typeof enUSResources.translation;
+
+export type TranslationKey = RecursiveKeyOf<TranslationResources>;
+
+const resources = {
+   en: enUSResources,
+};
+
+declare module "i18next" {
+   interface CustomTypeOptions {
+      resources: typeof resources;
+   }
+}
+
+i18n.init({
+   resources,
+   supportedLngs: ["en"],
+   defaultNS: "translation",
+   load: "languageOnly",
+   fallbackLng: "en",
+});
+
+export default i18n;
+
+export function translate(key: TranslationKey) {
+   const result = i18n.t(key);
+   if (result === key) {
+      console.warn(`Translation key not found: ${key}`);
+   }
+   return result;
+}
