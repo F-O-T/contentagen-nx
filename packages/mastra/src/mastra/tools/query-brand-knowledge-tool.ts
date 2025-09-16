d5079f249205a4d053d9c0ba200729df860499b8
Manoel <manoel.carvalhoneto@tutamail.com>
2025-09-16 06:54:01 -0300

    feat(chroma-db): Add brand and competitor knowledge collections feat(mastra): Add tools for brand and competitor information

--- packages/mastra/src/mastra/tools/query-brand-knowledge-tool.ts
+++ packages/mastra/src/mastra/tools/query-brand-knowledge-tool.ts
@@ -4,25 +4,24 @@ import { queryCollection, getCollection } from "@packages/chroma-db/helpers";
 import { createTool } from "@mastra/core/tools";
 import { z } from "zod";
 
-export const queryCompetitorFeatureInformation = createTool({
-   id: "query-competitor-feature-information",
-   description: "Query the competitor feature information",
+export const queryBrandKnowledgeTool = createTool({
+   id: "query-brand-knowledge",
+   description: "Query the brand knowledge rag",
    inputSchema: z.object({
-      organizationId: z.string().describe("The organization ID"),
+      agentId: z.string().describe("The agent ID"),
       keywords: z.array(z.string()).describe("The keywords to search for"),
    }),
    execute: async ({ context }) => {
-      const { organizationId, keywords } = context;
+      const { agentId, keywords } = context;
 
       const chroma = getChromaClient();
       try {
-         const collection = await getCollection(chroma, "CompetitorKnowledge");
+         const collection = await getCollection(chroma, "AgentKnowledge");
 
          const chunks = await queryCollection(collection, {
             nResults: 30,
             where: {
-               organizationId: organizationId,
-               type: "feature",
+               agentId: agentId,
             },
             queryTexts: keywords,
          });
