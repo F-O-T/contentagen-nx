b603c186f2e96deaea1327c5ec05c8788315b355
Manoel <manoel.carvalhoneto@tutamail.com>
2025-09-27 18:47:45 -0300

    feat(server): Add create new content workflow to bullboard

--- packages/agents/src/mastra/workflows/content/create-new-changelog-workflow.ts
+++ packages/agents/src/mastra/workflows/content/create-new-changelog-workflow.ts
@@ -130,6 +130,12 @@ const ContentReviewerStepOutputSchema =
          .array(z.string())
          .describe("The associeated keywords of the content"),
       sources: z.array(z.string()).describe("The sources found on the search"),
+      metaDescription: z
+         .string()
+         .describe(
+            "The meta description, being a SEO optmizaed description of the article",
+         ),
+      editor: editorType,
    }).omit({
       competitorIds: true,
       organizationId: true,
@@ -162,6 +168,7 @@ final:${editor}
                rating: true,
                reasonOfTheRating: true,
                keywords: true,
+               metaDescription: true,
             }),
          },
       );
@@ -176,11 +183,13 @@ final:${editor}
       return {
          rating: result.object.rating,
          reasonOfTheRating: result.object.reasonOfTheRating,
+         metaDescription: result.object.metaDescription,
          userId,
          agentId,
          contentId,
          request,
          keywords: result.object.keywords,
+         editor,
          sources: ["Your changelog"],
       };
    },
