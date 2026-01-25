6b2fb7a87bdebe65573eb68b7b06b88d3ccf747d
Manoel <manoel.carvalhoneto@tutamail.com>
2026-01-24 23:05:20 -0300

    fix

--- apps/web/.output/server/_chunks/_libs/@radix-ui/react-use-layout-effect.mjs
+++
@@ -1,6 +0,0 @@
-import { r as reactExports } from "../../../_libs/react.mjs";
-var useLayoutEffect2 = globalThis?.document ? reactExports.useLayoutEffect : () => {
-};
-export {
-  useLayoutEffect2 as u
-};
