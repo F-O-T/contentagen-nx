20dd3cb4eb2bb24aeb7549067cdc771ec8830f12
Manoel <manoel.carvalhoneto@tutamail.com>
2026-02-07 00:49:44 -0300

    chore(environment): remove parseEnv tests

--- packages/environment/src/helpers.ts
+++ packages/environment/src/helpers.ts
@@ -1,21 +1,5 @@
-import { z } from "zod";
-import { AppError } from "@packages/utils/errors";
-
 export const isClientProduction = import.meta.env.PROD;
 export const isProduction = process.env.NODE_ENV === "production";
-
-export function parseEnv<T extends z.ZodTypeAny>(
-   env: NodeJS.ProcessEnv,
-   schema: T,
-): z.infer<T> {
-   const result = schema.safeParse(env);
-   if (!result.success) {
-      throw AppError.validation("Invalid environment variables", {
-         data: result.error.format(),
-      });
-   }
-   return result.data;
-}
 
 export const getDomain = () => {
    if (process.env.APP_URL) {
