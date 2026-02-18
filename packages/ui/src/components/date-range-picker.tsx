48f5fea2f25053ce60477512cd866345063e6440
Manoel Neto <64708198+Yorizel@users.noreply.github.com>
2026-02-18 14:38:50 +0000

    style(web): Improve date range picker and calendar styling (#557)
    * style(web): Improve date range picker and calendar styling
    
    * style(date-range-picker): Add divider to the second month panel

--- packages/ui/src/components/date-range-picker.tsx
+++ packages/ui/src/components/date-range-picker.tsx
@@ -33,7 +33,7 @@ function CalendarDropdown({
          }}
          value={String(value)}
       >
-         <SelectTrigger className="h-7 w-auto min-w-[70px] border-input text-sm font-medium gap-1 focus:ring-0 focus:ring-offset-0 shadow-none">
+         <SelectTrigger className="h-7 w-auto min-w-[70px] border-input text-sm font-medium gap-1 focus:ring-0 focus:ring-offset-0 shadow-none cursor-pointer">
             <SelectValue />
          </SelectTrigger>
          <SelectContent>
@@ -137,7 +137,10 @@ export function DateRangePicker({
                captionLayout="dropdown"
                classNames={{
                   months: "flex gap-0 flex-col md:flex-row relative",
-                  month: "p-2 [&:not(:first-child)]:border-l",
+                  // react-day-picker v9 renders a <nav> as the 1st child of the months container,
+                  // so month panels start at position 2. n+3 targets the 2nd month panel (position 3)
+                  // to add a divider. If upgrading react-day-picker, verify nav placement hasn't changed.
+                  month: "flex flex-col w-full gap-4 px-2 pb-2 [&:nth-child(n+3)]:border-l",
                }}
                components={{ Dropdown: CalendarDropdown }}
                fromYear={2020}
